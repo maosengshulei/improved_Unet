d2d431cf791f016f3e750d196b7101dec165512b
maosengshulei <maosengshulei@qq.com>
2018-06-05 14:05:32 +0800

    add_ppm_deepsup

--- resnet_unet.py
+++ resnet_unet.py
@@ -460,6 +460,66 @@ class PPMBilinear(nn.Module):
 
         return x
 
+class PPMBilinearDeepsup(nn.Module):
+    def __init__(self, num_class=150, fc_dim=2048,
+                 use_softmax=False, pool_scales=(1, 2, 3, 6)):
+        super(PPMBilinearDeepsup, self).__init__()
+        self.use_softmax = use_softmax
+
+        self.ppm = []
+        for scale in pool_scales:
+            self.ppm.append(nn.Sequential(
+                nn.AdaptiveAvgPool2d(scale),
+                nn.Conv2d(fc_dim, 512, kernel_size=1, bias=False),
+                nn.BatchNorm2d(512),
+                nn.ReLU(inplace=True)
+            ))
+        self.ppm = nn.ModuleList(self.ppm)
+        self.cbr_deepsup = conv3x3_bn_relu(fc_dim // 2, fc_dim // 4, 1)
+
+        self.conv_last = nn.Sequential(
+            nn.Conv2d(fc_dim+len(pool_scales)*512, 512,
+                      kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.1),
+            nn.Conv2d(512, num_class, kernel_size=1)
+        )
+        self.conv_last_deepsup = nn.Conv2d(fc_dim // 4, num_class, 1, 1, 0)
+        self.dropout_deepsup = nn.Dropout2d(0.1)
+
+    def forward(self, conv_out, segSize=None):
+        conv5 = conv_out[-1]
+
+        input_size = conv5.size()
+        ppm_out = [conv5]
+        for pool_scale in self.ppm:
+            ppm_out.append(nn.functional.upsample(
+                pool_scale(conv5),
+                (input_size[2], input_size[3]),
+                mode='bilinear'))
+        ppm_out = torch.cat(ppm_out, 1)
+
+        x = self.conv_last(ppm_out)
+        x=nn.functional.upsample(x,scale_factor=16,mode='bilinear')
+        '''
+        if self.use_softmax:  # is True during inference
+            x = nn.functional.upsample(
+                x, size=segSize, mode='bilinear', align_corners=False)
+            x = nn.functional.softmax(x, dim=1)
+            return x
+        '''
+        # deep sup
+        conv4 = conv_out[-2]
+        _ = self.cbr_deepsup(conv4)
+        _ = self.dropout_deepsup(_)
+        _ = self.conv_last_deepsup(_)
+        _=nn.functional.upsample(_,scale_factor=16,mode='bilinear')
+        
+
+        return (x, _)
+
+
 
 class Loss:
     def __init__(self, dice_weight=1):
