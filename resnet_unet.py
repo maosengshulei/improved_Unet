afdabb443eb7df5d774a53d426957c9ed5e38227
maosengshulei <maosengshulei@qq.com>
2018-06-09 16:13:52 +0800

    fix_bugs

--- resnet_unet.py
+++ resnet_unet.py
@@ -320,7 +320,7 @@ class C1BilinearDeepSup(nn.Module):
         return (x, _)
 
 class C1Bilinear(nn.Module):
-	#dialated8 and upsample8
+    #dialated8 and upsample8
     def __init__(self, num_class=1, fc_dim=2048, use_softmax=False):
         super(C1Bilinear, self).__init__()
         self.use_softmax = use_softmax
@@ -345,7 +345,7 @@ class C1Bilinear(nn.Module):
         return x
 
 class C2Bilinear(nn.Module):
-	#no dialation and u-net decoder
+    #no dialation and u-net decoder
     def __init__(self,num_class=1,num_filters=32,is_deconv=False):
         super(C2Bilinear,self).__init__()
         self.center = DecoderBlockV2(2048, num_filters * 8 * 2, num_filters * 8,is_deconv)
@@ -373,7 +373,7 @@ class C2Bilinear(nn.Module):
 
 
 class C2Bilinearwithastorous16(nn.Module):
-	#dialated16 and u-net decoder
+    #dialated16 and u-net decoder
     def __init__(self,num_class=1,num_filters=32,is_deconv=False):
         super(C2Bilinearwithastorous16,self).__init__()
         self.center = DecoderBlockV2(2048, num_filters * 8 * 2, num_filters * 8,is_deconv)
@@ -401,7 +401,7 @@ class C2Bilinearwithastorous16(nn.Module):
         return x
 
 class C2Bilinearwithastorous8(nn.Module):
-	#dialated8 and u-net decoder
+    #dialated8 and u-net decoder
     def __init__(self,num_class=1,num_filters=32,is_deconv=False):
         super(C2Bilinearwithastorous8,self).__init__()
         self.center = DecoderBlockV2(2048, num_filters * 8 * 2, num_filters * 8,is_deconv)
@@ -612,7 +612,7 @@ class UPerNet(nn.Module):
         x = self.conv_last(fusion_out)
 
         x=nn.functional.upsample(
-        	x,scale_factor=4,mode='bilinear')
+            x,scale_factor=4,mode='bilinear')
         '''
         if self.use_softmax:  # is True during inference
             x = nn.functional.upsample(
@@ -621,7 +621,7 @@ class UPerNet(nn.Module):
             return x
 
         x = nn.functional.log_softmax(x, dim=1)
-		'''
+        '''
         return
 class deep_residual_unet(nn.Module):
 
@@ -673,36 +673,36 @@ class deep_residual_unet(nn.Module):
 
 class RCLblock(nn.Module):
     def __init__(self,inplanes,planes):
-	super(RCLblock,self).__init__()
- 	self.conv1=nn.Sequential(conv3x3(inplanes,planes),nn.BatchNorm2d(planes))
-	self.rcl=nn.Sequential(conv3x3(planes,planes),nn.BatchNorm2d(planes))
-	self.bn=nn.BatchNorm2d(planes)
+        super(RCLblock,self).__init__()
+        self.conv1=nn.Sequential(conv3x3(inplanes,planes),nn.BatchNorm2d(planes))
+        self.rcl=nn.Sequential(conv3x3(planes,planes),nn.BatchNorm2d(planes))
+        self.bn=nn.BatchNorm2d(planes)
 
 
     def forward(self,input_map):
-	conv1=self.conv1(input_map)
-	conv2=self.rcl(conv1)
-	conv2 += conv1
-	conv2=self.bn(conv2)
-	conv3=self.rcl(conv2)
-	conv3 += conv1
-	conv3=self.bn(conv3)
-	x=nn.ReLU(conv3,inplace=True)
-	return x
+        conv1=self.conv1(input_map)
+        conv2=self.rcl(conv1)
+        conv2 += conv1
+        conv2=self.bn(conv2)
+        conv3=self.rcl(conv2)
+        conv3 += conv1
+        conv3=self.bn(conv3)
+        x=nn.ReLU(conv3,inplace=True)
+        return x
 
 class RCL_Unet(nn.Module):
     def __init__(self,num_class=1,planes=32):
-	super(RCL_Unet,self).__init__()
-	self.pool = nn.MaxPool2d(2, 2)
-	self.center=nn.Sequential(
+        super(RCL_Unet,self).__init__()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.center=nn.Sequential(
             RCLblock(2048,planes*8)
             nn.MaxPool2d(2,2)
-			)
-	self.dec5=RCLblock(2048+planes*8,planes*8)
-	self.dec4=RCLblock(1024+planes*8,planes*4)
-	self.dec3=RCLblock(512+planes*4,planes*4)
-	self.dec2=RCLblock(256+planes*4,planes*2)
-	self,dec1=RCLblock(planes*2,planes)
+            )
+        self.dec5=RCLblock(2048+planes*8,planes*8)
+        self.dec4=RCLblock(1024+planes*8,planes*4)
+        self.dec3=RCLblock(512+planes*4,planes*4)
+        self.dec2=RCLblock(256+planes*4,planes*2)
+        self,dec1=RCLblock(planes*2,planes)
         self.upsample2x=nn.Upsample(scale_factor=2, mode='bilinear')
 
         self.cbr = conv3x3_bn_relu(num_filters, num_filters, 1)
@@ -711,13 +711,13 @@ class RCL_Unet(nn.Module):
         self.conv_last = nn.Conv2d(num_filters,num_class, 1, 1, 0)
 
     def forward(self,conv_out):
-    	center=nn.upsample2x(self.center(conv_out[-1]))
-    	dec5=self.upsample2x(self.dec5(torch.cat([conv_out[-1],center],1)))
-    	dec4=self.upsample2x(self.dec4(torch.cat([conv_out[-2],dec5],1)))
-    	dec4=self.upsample2x(self.dec3(torch.cat([conv_out[-3],dec4],1)))
-    	dec2=self.upsample2x(self.dec2(torch.cat([conv_out[-4],dec3],1)))
-    	dec1=self.upsample2x(self.dec1(dec2))
-    	x=self.cbr(dec1)
+        center=nn.upsample2x(self.center(conv_out[-1]))
+        dec5=self.upsample2x(self.dec5(torch.cat([conv_out[-1],center],1)))
+        dec4=self.upsample2x(self.dec4(torch.cat([conv_out[-2],dec5],1)))
+        dec4=self.upsample2x(self.dec3(torch.cat([conv_out[-3],dec4],1)))
+        dec2=self.upsample2x(self.dec2(torch.cat([conv_out[-4],dec3],1)))
+        dec1=self.upsample2x(self.dec1(dec2))
+        x=self.cbr(dec1)
         x = self.conv_last(x)
         return x
 
